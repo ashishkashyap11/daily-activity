5612d5d0a9acc72a8a3e70f578ab265c83a28464
Ashish Kashyap <ashishkashyap.appu@gmail.com>
2026-09-14 14:27:51 +0530

    Time: 111 ms (52.56%), Space: 15.7 MB (24.2%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,42 +1,47 @@
 class Solution(object):
     def minWindow(self, s, t):
-
         if not s or not t or len(s) < len(t):
             return ""
 
-        # Frequency map for characters in t
-        need = [0] * 256
+        tmb = [0] * 256
         for char in t:
-            need[ord(char)] += 1
+            tmb[ord(char)] += 1
 
-        have = [0] * 256
-        
-        # Helper function to check if 'have' satisfies 'need'
-        def fun(have_vec, need_vec):
-            for i in range(256):
-                if have_vec[i] < need_vec[i]:
-                    return False
-            return True
+        required = 0
+        for i in range(256):
+            if tmb[i] > 0:
+                required += 1
 
+        smb = [0] * 256
         low = 0
-        res = float('inf')
-        start = -1
+        matched = 0
 
-        # Sliding window
+        min_len = float('inf')
+        min_start = -1
+
         for high in range(len(s)):
-            have[ord(s[high])] += 1
+            right_char = s[high]
+            smb[ord(right_char)] += 1
 
-            # Shrink the window as long as it contains all required characters
-            while fun(have, need):
-                length = high - low + 1
-                if res > length:
-                    res = length
-                    start = low
-                
-                have[ord(s[low])] -= 1
+            if tmb[ord(right_char)] > 0 and smb[ord(right_char)] == tmb[ord(right_char)]:
+                matched += 1
+
+            while matched == required:
+
+                current_len = high - low + 1
+                if current_len < min_len:
+                    min_len = current_len
+                    min_start = low
+
+                left_char = s[low]
+                smb[ord(left_char)] -= 1
+
+                if tmb[ord(left_char)] > 0 and smb[ord(left_char)] < tmb[ord(left_char)]:
+                    matched -= 1
+
                 low += 1
 
-        if res == float('inf'):
+        if min_len == float('inf'):
             return ""
-            
-        return s[start:start + res]
+        else:
+            return s[min_start : min_start + min_len]
