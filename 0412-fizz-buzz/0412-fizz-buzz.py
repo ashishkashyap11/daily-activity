155b63f73b11fc68fdf27e6ebc856f61547dc45e
Ashish Kashyap <ashishkashyap.appu@gmail.com>
2026-09-15 11:55:48 +0530

    Time: 0 ms (100%), Space: 13.4 MB (11.11%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,9 +2,7 @@ class Solution(object):
     def fizzBuzz(self, n):
         result=[]
         for i in range(1,n+1):
-            if i==1 or i==2:
-                result.append(str(i))
-            elif i%3==0 and i%5==0:
+            if i%3==0 and i%5==0:
                 result.append("FizzBuzz")
             elif i%3==0:
                 result.append("Fizz")
