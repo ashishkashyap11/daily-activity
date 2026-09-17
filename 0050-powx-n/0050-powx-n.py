2e3e93a9405f2b3e66f7bab5693d0b1161546c40
Ashish Kashyap <ashishkashyap.appu@gmail.com>
2026-09-17 13:19:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (87.27%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,15 @@
 class Solution:
-    def fpowr(self, x: float, n: int) -> float:
-        if n == 0:
+    def fpow(self,x,n):
+        if n==0:
             return 1
-        a = self.fpowr(x, n // 2)
-        if n % 2 != 0:
-            return a * a * x
-        else:
-            return a * a
-
+        a=self.fpow(x,n//2)
+        if n%2!=0:
+            return a*a*x
+        else :
+            return a*a
     def myPow(self, x: float, n: int) -> float:
-        if n >= 0:
-            return self.fpowr(x, n)
-        else:
-            return 1 / self.fpowr(x, -n)
+        if n>=0:
+            return self.fpow(x,n)
+        else :
+            return 1/self.fpow(x,n*(-1))
+
