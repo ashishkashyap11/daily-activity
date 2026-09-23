1977c3dfc914b10c92e7be45b06a7a688089d194
Ashish Kashyap <ashishkashyap.appu@gmail.com>
2026-09-23 11:40:31 +0530

    Time: 119 ms (19.78%), Space: 15 MB (63.96%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,33 +1,29 @@
 class Solution(object):
     def maxSubarraySumCircular(self, nums):
+        i = 0
+        maxend = nums[i]
+        maxans = nums[i]
+        minend = nums[i]
+        minans = nums[i]
+        totalsum = sum(nums)
 
-        i=0
-        ans=float('-inf')
-        maxend=nums[i]
-        maxans=nums[i]
-        minans=nums[i]
-        minend=nums[i]
-        totalsum=sum(nums)
+        for i in range(1, len(nums)):
+            v1 = maxend + nums[i]
+            v2 = nums[i]
 
-        for i in range(1,len(nums)):
+            v3 = minend + nums[i]
+            v4 = nums[i]
 
-            v1=maxend+nums[i]
-            v2=nums[i]
+            maxend = max(v1, v2)
+            maxans = max(maxans, maxend)
 
-            v3=minend+nums[i]
-            v4=nums[i]
+            minend = min(v3, v4)
+            minans = min(minans, minend)
 
-            maxend=max(v1,v2)
-            minend=min(v3,v4)
-            minans=min(minans,minend)
-            maxans=max(maxans,maxend)
-
-            kuli=totalsum-minans
-
-            ans=max(maxans,kuli)   
-
+        # 1. Handle all-negative edgecase first
         if maxans < 0:
             return maxans
 
-        return ans
-
+        # 2. Calculate the circular maximum sum once at the end
+        kuli = totalsum - minans
+        return max(maxans, kuli)
